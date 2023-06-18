df336698f235da6d24bb957a29f4e9342025235c
KostiZin <pancostian@gmail.com>
2023-06-18 18:25:56 -0400

    Update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,56 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const KEY = 'videoplayer-current-time';
+
+let currentTime = {
+  duration: 0,
+  percent: 0,
+  seconds: 0,
+};
+
+const { duration, percent, seconds } = currentTime;
+
+player.on('play', function () {
+  //   preventDefault();
+  console.log('played the video!');
+
+  player.on('timeupdate', function (evt) {
+    console.log(evt);
+    localStorage.setItem(KEY, JSON.stringify(evt));
+  });
+});
+
+// console.log(currentTime);
+
+getCurrentTime();
+
+function getCurrentTime() {
+  const time = JSON.parse(localStorage.getItem(KEY));
+  currentTime = time;
+  return currentTime;
+}
+console.log(getCurrentTime());
+
+player
+  .setCurrentTime(currentTime.seconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
