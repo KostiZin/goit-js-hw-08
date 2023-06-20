2ced1b0bfa4343e16c8b931daa32feed7e03e7d8
KostiZin <pancostian@gmail.com>
2023-06-20 17:01:48 -0400

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,42 +3,32 @@ import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
 const inputEmail = document.querySelector('input');
 const inputMessage = document.querySelector('textarea');
-const btnEl = document.querySelector('button');
 
 const KEY = 'feedback-form-state';
 
-let currentData = {
+// let currentData = {
+//   email: '',
+//   message: '',
+// };
+
+let currentData = JSON.parse(localStorage.getItem(KEY)) || {
   email: '',
   message: '',
 };
 
-// const { email, message } = currentData;
-
 formEl.addEventListener('input', throttle(handleEmail, 500));
+formEl.addEventListener('submit', handleSubmit);
 
-// formEl.addEventListener('submit', handleSubmit);
-// ????????????????????????????????????????
-
-function handleEmail(evt) {
+function handleEmail() {
   currentData.email = inputEmail.value.trim();
   currentData.message = inputMessage.value.trim();
   localStorage.setItem(KEY, JSON.stringify(currentData));
 }
 
-// function handleEmail(evt) {
-//   if (evt.target.nodeName === 'INPUT') {
-//     currentData.email = evt.target.value.trim();
-//   } else {
-//     currentData.message = evt.target.value.trim();
-//   }
-
-//   return localStorage.setItem(KEY, JSON.stringify(currentData));
-// }
-
-currentData = JSON.parse(localStorage.getItem(KEY)) || {
-  email: '',
-  message: '',
-};
+// currentData = JSON.parse(localStorage.getItem(KEY)) || {
+//   email: '',
+//   message: '',
+// };
 
 updateValue();
 
@@ -51,11 +41,13 @@ function updateValue() {
     inputMessage.value = '';
   }
 }
-// ????????????????????????????????????
-// function handleSubmit(evt) {
-//   evt.preventDefault();
-//   localStorage.removeItem(KEY);
-// }
-// ??????????????????????????????????????
 
-console.log(currentData);
+function handleSubmit(evt) {
+  if (inputEmail.value === '' || inputMessage.value === '') {
+    return alert('All fields must be completed!');
+  }
+  evt.preventDefault();
+  console.log(currentData);
+  formEl.reset();
+  localStorage.removeItem(KEY);
+}
